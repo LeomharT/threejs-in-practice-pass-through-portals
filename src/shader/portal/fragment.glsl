44c0ddcfcb6fe35431bb2b0f486a7a87586aa5d3
LeomharT <leo.liao.lzy@gmail.com>
2026-03-19 08:00:00 +0800

    feat: corner center

--- src/shader/portal/fragment.glsl
+++ src/shader/portal/fragment.glsl
@@ -4,5 +4,21 @@ uniform float uBorderRadius;
 
 void main() {
     vec3  color  = vec3(0.0);
+    float ratio  = 2.0 / 3.0;
+    float r = uBorderRadius;
+
+    vec2 uv    = vUv;
+         uv.y /= ratio;
+
+    vec2 center    = vec2(0.5);
+         center.y /= ratio;
+
+    vec2 corner = abs(uv - center);
+    vec2 p = corner - (center - r);
+
+    if(p.x > 0.0 && p.y > 0.0) {
+        if(length(p) > r) discard;
+    }
+     
     gl_FragColor = vec4(color, 1.0);
 }
